ee810cb7886a89e56fb01747cfb04ac2f6864a61
SunHaoOne <2000839@stu.neu.edu.cn>
2023-03-28 16:36:02 +0800

    add compile with staticlib examples

---
+++ compileWithStaticLib/build/CMakeFiles/main.dir/sum.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/main.dir/sum.cpp.o: \
+ /home/shy/Learn-Cpp-Compilation-and-Debugging/compileWithStaticLib/sum.cpp \
+ /usr/include/stdc-predef.h \
+ /home/shy/Learn-Cpp-Compilation-and-Debugging/compileWithStaticLib/sum.h
